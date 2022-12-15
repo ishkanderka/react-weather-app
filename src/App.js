e00e80a2f608b73a3dc705beeb9bddd6114c485d
ishkanderka <victoriashmidt@outlook.com>
2022-12-15 13:41:35 -0700

    Added GitHub url and personal web-site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://effulgent-taffy-f461a8.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Victoria Shmidt
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ishkanderka/React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
